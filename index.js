c688fa9755f987f50ef2f23dc2254abdc2cbcb56
Itsssammara <hoosenammara@gmail.com>
2023-11-08 11:06:44 +0200

    added variables and operators

---
+++ index.js
@@ -0,0 +1,14 @@
+let language = 'Javascript';
+console.log(language)
+let score = '10'
+console.log(score)
+
+let length = "10";
+let width = "5";
+console.log(length * width)
+
+let num1 = "10";
+let num2 = "4";
+console.log(num1 % num2)
+
+console.log(`the area of the rectangle is ${length * width} where it was calculated `);
