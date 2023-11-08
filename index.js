c8efad2e8859094660ed6ff41cd2fa18a45e122d
Itsssammara <hoosenammara@gmail.com>
2023-11-08 16:12:49 +0200

    added new text

--- index.js
+++ index.js
@@ -12,3 +12,34 @@ let num2 = "4";
 console.log(num1 % num2)
 
 console.log(`the area of the rectangle is ${length * width} where it was calculated `);
+let integer = 'Integers: 100, -60,';
+console.log(integer);
+typeof integer
+
+let decimal = 0.12
+let decimal2 = 0.004
+console.log(typeof decimal + "s: ", decimal + ", ", decimal2)
+typeof decimal
+
+let string = 'Strings: "I am a string"';
+console.log(string)
+typeof string
+
+let array = 'Array: stores multiple values and elements in one variable, for example boolean, objects, number.';
+console.log(array)
+typeof array
+
+let object = 'Object: a collection of properties, and a property is an association between a name (or key) and a value. Booleans, numbers, strings and functions.' 
+console.log(object)
+typeof object
+
+const arrays = 'Toyota, Volvo, BMW, 9, 5, 10';
+console.log(arrays)
+typeof arrays
+
+let number = null//null
+console.log(number)
+console.log(x); //undefined
+
+
+
